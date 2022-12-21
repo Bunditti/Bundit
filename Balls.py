415ffa153ee0a56b9241932d28bd05a4e96f6854
Bunditti <117719861+Bunditti@users.noreply.github.com>
2022-12-21 10:09:34 +0700

    Add files via upload

---
+++ Balls.py
@@ -0,0 +1,9 @@
+question1 = input("floats in water? (Y/N)")
+if question1 == "Y":
+    print("Fake")
+else: 
+    question2 = input("Can scratch glass? (Y/N)")
+    if question2 == "N":
+        print("Fake")
+    else: "Y"
+    print("Could be real")
