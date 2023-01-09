b099e0f72f6ef2ff7c0d76f20b9771f65d2e644e
Bunditti <117719861+Bunditti@users.noreply.github.com>
2023-01-09 15:42:40 +0700

    Update Balls.py

--- Balls.py
+++ Balls.py
@@ -1,3 +1,4 @@
+print("THE DIAMOND EXPERT SYSTEM")
 question1 = input("floats in water? (Y/N)")
 if question1 == "Y":
     print("Fake")
@@ -5,5 +6,12 @@ else:
     question2 = input("Can scratch glass? (Y/N)")
     if question2 == "N":
         print("Fake")
-    else: "Y"
-    print("Could be real")
+    else:
+        question3 = input("Does it conduct electricity? (Y/N)")
+        if question3 == "Y":
+            print("Fake")
+        else: "N"
+        print("Could be real")
+
+            
+    
